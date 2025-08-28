0812c5b258d3d4335f1ea2a9c52facd55fc77470
subcaptaintsubasa <tsubasa.hsty@gmail.com>
2025-08-29 00:53:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+// --- sw.js ---
+const CACHE_NAME = 'xisync-cache-v2';
+const urlsToCache = [
+    './soccer%20member.html', // URL encoded filename
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js'
+];
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames.map(cacheName => {
+                if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    );
+});
